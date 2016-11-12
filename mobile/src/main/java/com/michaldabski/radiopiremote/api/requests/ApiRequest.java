eb1270a0dbaa6bb33b7f293edddfdd8c19b2932d
michaldabski <contact@michaldabski.com>
2016-11-12 22:04:42 +0000

    Customize retry policy

--- mobile/src/main/java/com/michaldabski/radiopiremote/api/requests/ApiRequest.java
+++ mobile/src/main/java/com/michaldabski/radiopiremote/api/requests/ApiRequest.java
@@ -2,6 +2,7 @@ package com.michaldabski.radiopiremote.api.requests;
 
 import android.support.annotation.Nullable;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Response;
 import com.michaldabski.radiopiremote.api.ApiUrlBuilder;
 
@@ -11,5 +12,6 @@ import com.michaldabski.radiopiremote.api.ApiUrlBuilder;
 public class ApiRequest<T> extends GsonRequest<T> {
     public ApiRequest(int method, ApiUrlBuilder urlBuilder, String endpoint, @Nullable Class<T> responseClass, Response.ErrorListener errorListener, @Nullable GsonResponseListener<T> responseListener) {
         super(method, urlBuilder.getEndpointUrl(endpoint), responseClass, errorListener, responseListener);
+        setRetryPolicy(new DefaultRetryPolicy(10000, 3, 1.5f));
     }
 }
