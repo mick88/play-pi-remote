07e124a8f86dc21cc54d3b42e5e242a16e914634
michaldabski <contact@michaldabski.com>
2016-11-13 18:31:29 +0000

    Implement custom layout for queue items

--- mobile/src/main/java/com/michaldabski/radiopiremote/queue/QueueAdapter.java
+++ mobile/src/main/java/com/michaldabski/radiopiremote/queue/QueueAdapter.java
@@ -2,25 +2,36 @@ package com.michaldabski.radiopiremote.queue;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.michaldabski.radiopiremote.R;
 import com.michaldabski.radiopiremote.api.models.BaseMpdModel;
+import com.michaldabski.radiopiremote.api.models.Track;
 
 import java.util.List;
+
+import static com.michaldabski.radiopiremote.R.id.imgArt;
+import static com.michaldabski.radiopiremote.R.id.tvTitle;
 
 /**
  * Created by Michal on 31/10/2016.
  */
 
 public class QueueAdapter extends ArrayAdapter<BaseMpdModel> {
+    private static class ViewHolder {
+        ImageView imgArt;
+        TextView tvTitle;
+    }
+
     Integer currentMpdId = null;
 
     public QueueAdapter(Context context, List<BaseMpdModel> items) {
-        super(context, android.R.layout.simple_list_item_1, items);
+        super(context, 0, items);
     }
 
     public void setCurrentMpdId(Integer currentMpdId) {
@@ -30,17 +41,27 @@ public class QueueAdapter extends ArrayAdapter<BaseMpdModel> {
 
     @NonNull
     @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-        final View view = super.getView(position, convertView, parent);
-        if (currentMpdId != null) {
-            final TextView textView = (TextView) view.findViewById(android.R.id.text1);
-            final BaseMpdModel item = getItem(position);
-            if (currentMpdId.equals(item.getMpdId())) {
-                textView.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_play, 0, 0, 0);
-            } else {
-                textView.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, 0, 0);
-            }
+    public View getView(int position, View view, @NonNull ViewGroup parent) {
+        final ViewHolder viewHolder;
+        if (view == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.item_queue, parent, false);
+            view.setTag(viewHolder = new ViewHolder());
+            viewHolder.tvTitle = (TextView) view.findViewById(tvTitle);
+            viewHolder.imgArt = (ImageView) view.findViewById(imgArt);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
         }
+
+        final BaseMpdModel item = getItem(position);
+
+
+        viewHolder.tvTitle.setText(item.getName());
+        if (currentMpdId != null && currentMpdId == item.getMpdId()) {
+            viewHolder.imgArt.setImageResource(R.drawable.ic_play);
+        } else {
+            viewHolder.imgArt.setImageResource(item instanceof Track ? R.drawable.ic_track : R.drawable.ic_radio);
+        }
+
         return view;
     }
 }
