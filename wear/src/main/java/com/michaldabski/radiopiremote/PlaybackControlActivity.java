91d18f94029f46a42d07fcda786c33be27713cd0
michaldabski <contact@michaldabski.com>
2016-11-14 21:37:58 +0000

    Use basic activity class

--- wear/src/main/java/com/michaldabski/radiopiremote/PlaybackControlActivity.java
+++ wear/src/main/java/com/michaldabski/radiopiremote/PlaybackControlActivity.java
@@ -1,13 +1,15 @@
 package com.michaldabski.radiopiremote;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class PlaybackControlActivity extends AppCompatActivity {
+public class PlaybackControlActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playback_control);
     }
+
+
 }
