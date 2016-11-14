62996542baacf8845315a3b8e4087191a8544137
michaldabski <contact@michaldabski.com>
2016-11-14 21:33:37 +0000

    Create playback control activity

---
+++ wear/src/main/java/com/michaldabski/radiopiremote/PlaybackControlActivity.java
@@ -0,0 +1,13 @@
+package com.michaldabski.radiopiremote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PlaybackControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playback_control);
+    }
+}
