f49650cbf08f7b510c20708e2147ab192fd7fc8e
michaldabski <contact@michaldabski.com>
2016-11-14 21:54:29 +0000

    Add refresh button

--- mobile/src/main/java/com/michaldabski/radiopiremote/queue/QueueFragment.java
+++ mobile/src/main/java/com/michaldabski/radiopiremote/queue/QueueFragment.java
@@ -3,6 +3,9 @@ package com.michaldabski.radiopiremote.queue;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
@@ -38,9 +41,26 @@ public class QueueFragment extends BaseFragment implements GsonResponseListener,
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
 
         final ImageLoader imageLoader = getPiRemoteApplication().getImageLoader();
         this.adapter = new QueueAdapter(getContext(), new ArrayList<BaseMpdModel>(), imageLoader);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.queue_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuRefresh:
+                fetchQueue();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Nullable
