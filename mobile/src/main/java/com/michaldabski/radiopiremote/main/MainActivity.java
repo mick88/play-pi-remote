6c464bc667f5ed34d98bc4f5eda7d51e9870df4a
michaldabski <contact@michaldabski.com>
2016-11-14 19:35:35 +0000

    Display version number in actionbar

--- mobile/src/main/java/com/michaldabski/radiopiremote/main/MainActivity.java
+++ mobile/src/main/java/com/michaldabski/radiopiremote/main/MainActivity.java
@@ -5,11 +5,13 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.michaldabski.radiopiremote.BaseActivity;
+import com.michaldabski.radiopiremote.BuildConfig;
 import com.michaldabski.radiopiremote.PiRemoteApplication;
 import com.michaldabski.radiopiremote.R;
 import com.michaldabski.radiopiremote.api.models.Status;
@@ -31,6 +33,9 @@ public class MainActivity extends BaseActivity implements GsonResponseListener<S
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setSubtitle("Version " + BuildConfig.VERSION_NAME);
 
         final SharedPreferences preferences = getPiRemoteApplication().getSharedPreferences();
         if (savedInstanceState == null && preferences.contains(PiRemoteApplication.PREF_ADDRESS) == false) {
